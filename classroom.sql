4ab03109a708aaaebfabbb17e1c0815cc5477131
NetworkCrab <network123crab@gmail.com>
2023-12-26 20:58:58 +0530

    modified

--- classroom.sql
+++ classroom.sql
@@ -1,14 +1,19 @@
-Create Database collage;
-
+Create database collage; 
 USE collage;
-
-CREATE TABLE student (
-	id INT PRIMARY KEY,
-	name VARCHAR(50),
-	age INT NOT NULL
+DROP DATABASE IF exists company;
+# Creating a table in the database
+create table student(
+	rollno int primary key,
+    name varchar(50)
 );
+show tables;
 
-INSERT INTO student VALUES(1, 'Biswarup', 23);
-INSERT INTO student VALUES(2, 'Jeet', 22);
+select * from student;
 
-SELECT * from student;
+INSERT INTO student
+(rollno, name)
+values
+(101, "karan"),
+(102, "arjun");
+
+
