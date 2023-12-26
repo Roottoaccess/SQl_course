7b168220e9c759eff6dba6151e7125bd99188650
NetworkCrab <network123crab@gmail.com>
2023-12-26 13:19:02 +0530

    This is the sql another practise

---
+++ practise2.sql
@@ -0,0 +1,21 @@
+# Creating the database
+create database iphone;
+
+use iphone;
+# creating the table name and inserting the value
+create table employee(
+id int primary key,
+name varchar (50),
+salary int (50)
+);
+
+# Seeing or viewing the table
+select * from employee;
+
+# Inserting the data to the table
+insert into employee
+(id , name, salary)
+values
+(1, "adam", 25000),
+(2, "bob" , 30000),
+(3, "casey", 40000);
